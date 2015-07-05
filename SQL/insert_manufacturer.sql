3b3a63f845ea9211f8c1c573c518b63820ce4b06
Moritz Horn <moritz.horn@gmail.com>
2015-07-05 20:37:18 +0200

    removed unused sql

--- SQL/insert_manufacturer.sql
+++
@@ -1,1 +0,0 @@
-INSERT INTO `cars`.`manufacturer` (`id_manufacturer`, `name`) VALUES ('1', 'Audi'), ('2', 'BMW'), ('3', 'Chevrolet'), ('4', 'Fiat'), ('5', 'Ford'), ('6', 'Jaguar'), ('7', 'Mazda'), ('8', 'Mercedes Benz'), ('9', 'Peugeot'), ('10', 'Renault'), ('11', 'Subaru'), ('12', 'Volvo');
